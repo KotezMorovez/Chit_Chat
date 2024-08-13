aefabee29a51355cb6b0ec2fc7721d41ee7b16e6
KotezMorovez <odevyatlovskaya@gmail.com>
2024-08-13 19:24:11 +0500

    Add chat list menu UI

--- app/src/main/java/com/example/chit_chat/ui/home/chat_list/adapter/ChatListAdapter.kt
+++ app/src/main/java/com/example/chit_chat/ui/home/chat_list/adapter/ChatListAdapter.kt
@@ -2,15 +2,20 @@ package com.example.chit_chat.ui.home.chat_list.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.chit_chat.R
 import com.example.chit_chat.databinding.ItemChatListBinding
 
-
 class ChatListAdapter(
-    private val onItemClickListener: (item: ChatItem) -> Unit
+    private val onItemClickListener: (item: ChatItem) -> Unit,
+    private val onLongPressListener: (
+        item: ChatItem,
+        itemView: View,
+        coordinates: Pair<Float, Float>
+    ) -> Unit
 ) : RecyclerView.Adapter<ChatListAdapter.ViewHolder>() {
     private var items: List<ChatItem> = listOf()
 
@@ -37,7 +42,10 @@ class ChatListAdapter(
 
     inner class ViewHolder(private val binding: ItemChatListBinding) :
         RecyclerView.ViewHolder(binding.root) {
+        @SuppressLint("ClickableViewAccessibility")
         fun bind(item: ChatItem) {
+            var lastX = 0f
+            var lastY = 0f
             with(binding) {
                 Glide.with(userAvatarImageView)
                     .load(item.userAvatar)
@@ -56,8 +64,19 @@ class ChatListAdapter(
                 userMessageTextView.invalidate()
                 userMessageTextView.requestLayout()
 
-                root.setOnClickListener {
+                itemLayout.setOnClickListener {
                     onItemClickListener.invoke(item)
+                }
+
+                itemLayout.setOnTouchListener { _, event ->
+                    lastX = event.x
+                    lastY = event.y
+                    false
+                }
+
+                itemLayout.setOnLongClickListener {
+                    onLongPressListener.invoke(item, it, Pair(lastX, lastY))
+                    true
                 }
             }
         }
